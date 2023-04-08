8bb072aa8225f2826ac6ae16b211763006039f25
Muhammad Huzaifa <muhammadhuzaifa457@gmail.com>
2023-04-08 16:53:04 +0500

    add book first fail test case

--- bookapi.test.js
+++ bookapi.test.js
@@ -1,9 +1,21 @@
-const {getBook} = require('./src/readingList')
-test('My list to be empty',()=>{
+const { getBook } = require('./src/readingList')
+test('My list to be empty', () => {
     //Arrange
     const assert = null;
     //Act
     const readingList = getBook();
     //Assert
     expect(assert).toBe(readingList);
+})
+
+test('add a first book', () => {
+    //Arrange
+    const book = { title: "Justuju k safr", author: 'Zeeshan ul hassan usmani', length: 345, year: 2021 };
+    const assert = 1;
+    //Act
+    addBook(book, readingDate, rating);
+    const result = numberRead();
+    //Assert
+    expect(assert).toBe(result);
+
 })
